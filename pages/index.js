734c63dd46aecdb8be3820fd3f7e2f9a9fb2b38d
jhon doe <jhon doe>
2023-01-26 14:35:14 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -25,10 +25,10 @@ const Index = (   ) => {
     const canvasEl = useRef(null);
     const colors = {
         purple: {
-          default: "rgba(149, 76, 233, 1)",
-          half: "#0000636b",
-          quarter: "rgba(149, 76, 233, 0.25)",
-          zero: "#534bae93"
+          default: "#1c2541b5",
+          half: "#1c2541b5",
+          quarter: "#1c2541b5",
+          zero: "#1c2541c8"
         },
         indigo: {
           default: "#311b92",
@@ -215,7 +215,7 @@ const Index = (   ) => {
                                             
                                         {/* linkedin  */}
                                         <Link target="_blank" href="https://www.linkedin.com/in/fernandopinango">
-                                            <button    className={classNames('btn btn-rounded-big btn-active m1', {'btn-inactive': skilsSize == 'open'})} >
+                                            <button    className={classNames('btn btn-rounded-big btn-active-top-card m1')} >
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                                                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
                                                     </svg>
@@ -224,7 +224,7 @@ const Index = (   ) => {
 
                                         {/* github */}
                                         <Link target="_blank" href="https://github.com/fernanj2323">
-                                            <button  href="https://www.linkedin.com/in/fernandopinango" taget="_blank"  className={classNames('btn btn-rounded-big  btn-active ', {'btn-inactive': skilsSize == 'open'})} >
+                                            <button  href="https://www.linkedin.com/in/fernandopinango" taget="_blank"  className={classNames('btn btn-rounded-big btn-active-top-card ')} >
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
                                                     <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
                                                     </svg>   
@@ -329,6 +329,8 @@ const Index = (   ) => {
             {/* second seccion */}
             <div className="row py-2">
 
+
+            {/* navs tabs */}
             {skilsSize == "open" ? (
                 <div className='display-flex'>   
                     <div onClick={setFavoriteSk} className={classNames('nav-page firts-page '   , {'active-page color-active': favoriteFirst == 'skils'}  ) } >Skills</div>
@@ -617,7 +619,7 @@ const Index = (   ) => {
                                             <div>
                                         
                                                 {/* full width */}
-                                                <button onClick={setFavoriteExperiences}  className={classNames('btn btn-rounded blues ', {'btn-active': skilsSize == 'open'})}   >
+                                                <button onClick={setFavoriteExperiences}  className={classNames('btn btn-rounded  blues ', {'btn-active': skilsSize == 'open'})}   >
 
 
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-angle-expand" viewBox="0 0 16 16">
@@ -677,7 +679,7 @@ const Index = (   ) => {
                        <div className="row">
                        <div className="col-md-12">
                            {/* className="card card-body "   */}
-                           <div  className={classNames(' card-second-color card card-body  animate__animated animate__fadeIn border-top-radius-0 border-bottom-radius-0' , {'bg-dark': theme == 'dark'})} >
+                           <div  className={classNames('card-active-color card card-body  animate__animated animate__fadeIn border-top-radius-0 border-bottom-radius-0' , {'bg-dark': theme == 'dark'})} >
                                <div className="row">
                                     {/* header */}
                                     <div className="row col-md-12 red" >
@@ -718,7 +720,7 @@ const Index = (   ) => {
                                            projects.map(({name,description,image}, i) =>(
                                            <div className="col-md-4 p-2" key={i}>
                                                {/* className="card h-100 " */}
-                                               <div   className={classNames(' card-active-color card card-body  animate__animated animate__fadeIn', {'bg-dark': theme == 'dark'})}>
+                                               <div   className={classNames('  card-second-color  card card-body  animate__animated animate__fadeIn border-top-radius-0 border-bottom-radius-0', {'bg-dark': theme == 'dark'})}>
                                                    <div className="overflow">
                                                        <img className='card-img-top' src={`/${image}`} alt=''/>
                                                    </div>
@@ -743,7 +745,7 @@ const Index = (   ) => {
                     <div className="col-md-12 ">
                                   
 
-                        <div  className={classNames('card card-body  card-active-color col-md-12  padding-top0  animate__animated animate__fadeIn border-top-radius-2 border-bottom-radius-0' , {'bg-dark': theme == 'dark'})} >
+                        <div  className={classNames(' card-active-color card card-body   col-md-12  padding-top0  animate__animated animate__fadeIn border-top-radius-2 border-bottom-radius-0' , {'bg-dark': theme == 'dark'})} >
 
                             <div className="row card-body">
                              {/* header */}
@@ -786,8 +788,8 @@ const Index = (   ) => {
                                     {
                                     projects.map(({name,description,image}, i) =>(
                                                 <div className="col-md-4 p-2" key={i}>
-                                                    {/* className="card h-100 " */}
-                                                    <div   className={classNames(' card-second-color card card-body  animate__animated animate__fadeIn', {'bg-dark': theme == 'dark'})}>
+                                             
+                                                    <div   className={classNames('card-second-color border-top-radius-0 border-bottom-radius-0  card card-body  animate__animated animate__fadeIn', {'bg-dark': theme == 'dark'})}>
                                                         <div className="overflow">
                                                             <img className='card-img-top' src={`/${image}`} alt=''/>
                                                         </div>
